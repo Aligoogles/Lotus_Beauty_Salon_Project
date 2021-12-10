ce0f8a6be6d2bdd15731ef232f36ddc1769eca44
Ali <ali.galway2002@gmail.com>
2021-12-10 19:46:24 +0000

    Added functionality to the star rating on index page
    Added onclick atribute  to the star elements
    Onclick calls js function which takes a unique ID for each element as a parameter
    function sets the star rating based on the parameter
    
    Signed-off-by: Ali <ali.galway2002@gmail.com>

--- scripts/a_index_script.js
+++ scripts/a_index_script.js
@@ -1,6 +1,41 @@
 /* Start Index page */
 
 
+function toggleStar(number) {
+    starID = "a-star-"+number;
+    var start;
+    var end;
+    if(number >=1 && number <= 5){
+        start = 1;
+        end = 5;
+    }
+    else if(number >=6 && number <= 10){
+        start = 6;
+        end = 10;
+    }
+    else{
+        start = 11;
+        end = 15;
+    }
+
+    for(i=start;i<=end;i++){
+        starID = "a-star-"+i;
+        document.getElementById(starID).className = "fa fa-star ";
+    }
+
+    if(document.getElementById(starID).className == "fa fa-star "){
+        for(i=start;i<=number;i++){
+            starID = "a-star-"+i;
+            document.getElementById(starID).className = "fa fa-star checked";
+        }
+    }
+
+  }
+
+ 
+
+  
+
 
 /*Start Footer - Aelita (21113131)*/
 function scrollToTop() {
