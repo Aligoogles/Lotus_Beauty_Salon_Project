cea35f722554a3dabdb40b03188629b51284d2ac
Ali <ali.galway2002@gmail.com>
2021-12-11 23:09:03 +0000

    Updated aelitaJavaScript file
    Added js code for contact form
    
    Signed-off-by: Ali <ali.galway2002@gmail.com>

--- scripts/aelitasJavaScript.js
+++ scripts/aelitasJavaScript.js
@@ -32,6 +32,65 @@ function toggleStar(number) {
 }
 /* End of star functionality */
 
+/* Contact form  Start*/
+function validateForm() {
+    let name = document.forms["a-contact-form"]["a-fullname"].value;
+    let email = document.forms["a-contact-form"]["a-email"].value;
+    let number = document.forms["a-contact-form"]["a-ph"].value;
+    
+    //test name for two words
+    if(name.indexOf(" ") != -1){
+        name = name.split(" ");
+        if(!name[1].length >= 1){
+            alert("Name must contain two or more words");
+            return false;
+        }
+    }else{
+        alert("Name must contain two or more words");
+        return false;
+    }
+
+    //test email for @, then test for period, then test for extension
+    if(email.indexOf("@") != -1){
+        email = email.split("@");
+        if(email[1].indexOf(".") != -1){
+            email = email[1].split(".");
+            if(!email[1].length > 0){
+                alert("Email must have an extension");
+                return false;
+            }
+        }
+        else{
+            alert("Email must contain a period");
+            return false;
+        }
+    }
+    else{
+        alert("Email must contain @ symbol");
+        return false;
+    }
+
+    //test phone number is only numbers then test if between 10 & 12 digits long
+    for( let i = 0; i < number.length; i++){
+        if(isNaN(number.charAt(i)) && !(number.charAt(i) === " ") ){
+            alert("Phone number must contain only digits");
+            return false;
+        }
+    }
+    if(number.length < 10){
+        alert("Not enough digits in phone number");
+        return false;
+    }
+    else if(number.length > 12){
+        alert("Too many digits in phone number");
+        return false;
+    }
+
+    //if all successful notify the user
+    alert("Information has been validated");
+    return true; 
+  } /* Contact form End */
+
 
 
 /*Start of hamburger menu 
