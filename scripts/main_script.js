f45da3a6980d9bd66bd1ce58c231a3daccaaaff1
Ali <ali.galway2002@gmail.com>
2021-12-11 19:32:28 +0000

    Updated main js file
    amended the main styles sheet
    
    Signed-off-by: Ali <ali.galway2002@gmail.com>

--- scripts/main_script.js
+++ scripts/main_script.js
@@ -1,4 +1,46 @@
-/*Aelitha (21113131)JavaScript*/
+/*Aelita (21113131)JavaScript*/
+
+/* Start Index page */
+
+/* Star functionality */
+function toggleStar(number) {
+    starID = "a-star-"+number;
+    var start;
+    var end;
+    if(number >=1 && number <= 5){
+        start = 1;
+        end = 5;
+    }
+    else if(number >=6 && number <= 10){
+        start = 6;
+        end = 10;
+    }
+    else{
+        start = 11;
+        end = 15;
+    }
+
+    for(i=start;i<=end;i++){
+        starID = "a-star-"+i;
+        document.getElementById(starID).className = "fa fa-star ";
+    }
+
+    if(document.getElementById(starID).className == "fa fa-star "){
+        for(i=start;i<=number;i++){
+            starID = "a-star-"+i;
+            document.getElementById(starID).className = "fa fa-star checked";
+        }
+    }
+}
+/* End of star functionality */
+
+/*Start Footer - Aelita (21113131)*/
+function scrollToTop() {
+    window.scrollTo(0, 0);
+}
+/* End Footer */
+
+
 
 /*Ross (21132305) JavaScript*/
 //Javascript for the Form
